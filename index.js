bc5ddbe6271a24a848dbd0af26e3277b638c02ca
jolly <vasanth_r@outlook.com>
2023-03-06 19:54:17 +0530

    Displays files in order, as long as directory items are unchanged using fn.lstat(). * Uses an array along with fs.lstat Fn. * Lists directory items in order. * Has access to additional functionality fs.lstat Fn offers. * Changes index.js.

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const fs = require('fs');
 fs.readdir(process.cwd(), (error, fileNames) => {
     // readdir first argument is path
     // Callback Fn details below   
-    // Takes Error, Files as arguments
-    // Returns an Err object if something is wrong or will return null if all ok
+    // Takes Error, array of Files as arguments
+    // Returns an Err object if something is wrong or null if all ok
     // second argument: array of string with the names of files in the path passed
     if (error) {
         // if error object is defined, then Error handling code
@@ -15,14 +15,24 @@ fs.readdir(process.cwd(), (error, fileNames) => {
         // return;
         // throw new Error(err);
     }
-    // console.log(fileNames);
-    // Out of order response approach
+    // fs.lstat and array combo
+    const allStats = Array(fileNames.length).fill(null);
     for (let fileName of fileNames) {
+        // Files are unique, so, no issues (here)
+        const index = fileNames.indexOf(fileName);
         fs.lstat(fileName, (error, stats) => {
             if (error) {
                 console.log(error);
             }
-            console.log(fileName, stats.isFile());
+            allStats[index] = stats;
+            // ready returns true if all items in the array are not null
+            const ready = allStats.every(stats => stats);
+            if (ready) {
+                allStats.forEach((stats, index) => {
+                    // Output of nls command to user
+                    console.log(fileNames[index], stats.isFile());
+                });
+            }
         });
     }
 });
