2687febc362a36fc0327c5d3e0ec5d7b97a608da
jolly <vasanth_r@outlook.com>
2023-03-06 13:30:14 +0530

    Adds Fn fs.readdir() to display the contents of the path passed as argument. * Changes index.js.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // Project: To list the contents of current directory
 // To use FileSystem from POSIX
 const fs = require('fs');
+// Getting files from directory
+fs.readdir('.', (error, fileNames) => {
+    // Takes Error, Files as arguments
+    // Returns an Err object if something is wrong or will return null if all ok
+    // second argument: array of string with the names of files in the path passed
+    if (error) {
+        // if error object is defined, then Error handling code
+        console.log(error);
+        // return;
+        // throw new Error(err);
+    }
+    console.log(fileNames);
+});
