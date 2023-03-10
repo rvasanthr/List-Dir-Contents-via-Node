6d916217e6c48d9d8284198d0303e1ed123caa7d
jolly <vasanth_r@outlook.com>
2023-03-10 14:52:58 +0530

    Uses Promise and Lstat combo to display the contents of the directory and their respective file or not file status. * Changes index.js.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 // Project: To list the contents of current directory
 // To use FileSystem from POSIX
+const { rejects } = require('assert');
 const fs = require('fs');
+const { resolve } = require('path');
 // Getting files from directory
-fs.readdir(process.cwd(), (error, fileNames) => {
+fs.readdir(process.cwd(), async (error, fileNames) => {
     // readdir first argument is path
     // Callback Fn details below   
     // Takes Error, array of Files as arguments
@@ -12,27 +14,18 @@ fs.readdir(process.cwd(), (error, fileNames) => {
     if (error) {
         // if error object is defined, then Error handling code
         console.log(error);
-        // return;
-        // throw new Error(err);
     }
-    // fs.lstat and array combo
-    const allStats = Array(fileNames.length).fill(null);
     for (let fileName of fileNames) {
-        // Files are unique, so, no issues (here)
-        const index = fileNames.indexOf(fileName);
-        fs.lstat(fileName, (error, stats) => {
-            if (error) {
-                console.log(error);
-            }
-            allStats[index] = stats;
-            // ready returns true if all items in the array are not null
-            const ready = allStats.every(stats => stats);
-            if (ready) {
-                allStats.forEach((stats, index) => {
-                    // Output of nls command to user
-                    console.log(fileNames[index], stats.isFile());
-                });
-            }
-        });
+        const stats = await lstat(fileName);
+        console.log(fileName, stats.isFile());
     }
 });
+// lstat and promise combo 1
+const lstat = fileName => {
+    return new Promise((resolve, reject) => {
+        fs.lstat(fileName, (error, stats) => {
+            if (error) reject(error);
+            resolve(stats);
+        });
+    });
+}
