3826b0834996d684ac19a21ca7419e2ab87200f4
jolly <vasanth_r@outlook.com>
2023-03-10 14:58:21 +0530

    Updates error handling via try / catch statement inside the for loop of fs.readdir Fn. * Changes index.js.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 // Project: To list the contents of current directory
 // To use FileSystem from POSIX
-const { rejects } = require('assert');
+// const { rejects } = require('assert');
 const fs = require('fs');
-const { resolve } = require('path');
+// const { resolve } = require('path');
 // Getting files from directory
 fs.readdir(process.cwd(), async (error, fileNames) => {
     // readdir first argument is path
@@ -11,13 +11,14 @@ fs.readdir(process.cwd(), async (error, fileNames) => {
     // Takes Error, array of Files as arguments
     // Returns an Err object if something is wrong or null if all ok
     // second argument: array of string with the names of files in the path passed
-    if (error) {
-        // if error object is defined, then Error handling code
-        console.log(error);
-    }
     for (let fileName of fileNames) {
-        const stats = await lstat(fileName);
-        console.log(fileName, stats.isFile());
+        // Processing errors if encountered for each async and await response
+        try {
+            const stats = await lstat(fileName);
+            console.log(fileName, stats.isFile());
+        } catch (error) {
+            console.log(error);
+        }
     }
 });
 // lstat and promise combo 1
